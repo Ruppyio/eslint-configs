2ba36238e56f88f65129e140e06680f64558f453
LS <latifsulistyo.me@gmail.com>
2020-11-04 23:36:58 +0700

    fix(ruppy-ts): resolve d.ts override not picking up

--- packages/eslint-config-ruppy-ts/index.js
+++ packages/eslint-config-ruppy-ts/index.js
@@ -5,47 +5,20 @@ module.exports = {
   overrides: [
     {
       files: ['**/*.ts?(x)'],
-      extends: [
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
-        './rules/typescript-extensions',
-        'prettier/@typescript-eslint',
-      ],
-      parserOptions: {
-        warnOnUnsupportedTypeScriptVersion: true,
-        project: 'tsconfig.json',
-      },
-      settings: { 'import/resolver': { typescript: {} } },
+      extends: ['./ts'],
+    },
+    {
+      files: ['**/*.d.ts'],
+      extends: ['./ts'],
       rules: {
-        // TypeScript's `noFallthroughCasesInSwitch` option is more robust
-        'default-case': 'off',
+        'import/no-extraneous-dependencies': 'off',
 
-        // TypeScript specific rules (preferences)
-        // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/README.md#supported-rules
-        '@typescript-eslint/no-empty-interface': 'off',
-        '@typescript-eslint/explicit-module-boundary-types': 'off',
-        '@typescript-eslint/consistent-type-assertions': [
-          'warn',
-          {
-            assertionStyle: 'as',
-            objectLiteralTypeAssertions: 'allow-as-parameter',
-          },
-        ],
+        '@typescript-eslint/ban-types': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/triple-slash-reference': 'off',
+
+        '@typescript-eslint/no-use-before-define': 'off',
       },
-      overrides: [
-        {
-          files: ['**/*.d.ts'],
-          rules: {
-            'import/no-extraneous-dependencies': 'off',
-
-            '@typescript-eslint/ban-types': 'off',
-            '@typescript-eslint/no-explicit-any': 'off',
-            '@typescript-eslint/triple-slash-reference': 'off',
-
-            '@typescript-eslint/no-use-before-define': 'off',
-          },
-        },
-      ],
     },
   ],
 };
---
+++ packages/eslint-config-ruppy-ts/ts.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    './rules/typescript-extensions',
+    'prettier/@typescript-eslint',
+  ],
+  parserOptions: {
+    warnOnUnsupportedTypeScriptVersion: true,
+    project: 'tsconfig.json',
+  },
+  settings: { 'import/resolver': { typescript: {} } },
+  rules: {
+    // TypeScript's `noFallthroughCasesInSwitch` option is more robust
+    'default-case': 'off',
+
+    // TypeScript specific rules (preferences)
+    // https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/README.md#supported-rules
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/consistent-type-assertions': [
+      'warn',
+      {
+        assertionStyle: 'as',
+        objectLiteralTypeAssertions: 'allow-as-parameter',
+      },
+    ],
+  },
+};
